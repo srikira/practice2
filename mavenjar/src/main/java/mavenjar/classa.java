5b0997ebada0f438d3793160fbca213492519ffb
QAP36 <QAP36@QAP36-PC>
2018-10-29 13:20:09 +0530

    add

---
+++ mavenjar/src/main/java/mavenjar/classa.java
@@ -0,0 +1,17 @@
+package mavenjar;
+
+public class classa 
+{
+	public void method1()
+{
+	System.out.println("class");
+}
+	public void method2()
+{
+	System.out.println("classhk");
+}
+public void method3()
+{
+	System.out.println("classuiuio");
+}
+}
